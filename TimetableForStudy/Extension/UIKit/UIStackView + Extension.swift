5f413fa07eca70ef8a65cd4084d6014ad3df7b51
Artem <artem-orlov92@bk.ru>
2023-05-09 23:04:24 +0300

    add stack

---
+++ TimetableForStudy/Extension/UIKit/UIStackView + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIStackView + Extension.swift
+//  TimetableForStudy
+//
+//  Created by Артем Орлов on 09.05.2023.
+//
+
+import UIKit
+
+extension UIStackView {
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat, distribution: UIStackView.Distribution) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+        self.distribution = distribution
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
