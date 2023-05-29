8ded21f79b180fb8df69bf7b97abeafa83a175c3
Artem <artem-orlov92@bk.ru>
2023-05-30 00:18:14 +0300

    add Model for ContactModel realm

--- TimetableForStudy/Model/Realm/ContactModel.swift
+++ TimetableForStudy/Model/Realm/ContactModel.swift
@@ -10,4 +10,9 @@ import RealmSwift
 
 class ContactModel: Object {
     
+    @Persisted var contactName = "Unknown"
+    @Persisted var contactPhone = "Unknown"
+    @Persisted var contactMain = "Unknown"
+    @Persisted var contactType = "Unknown"
+    @Persisted var contactImage: Date?
 }
